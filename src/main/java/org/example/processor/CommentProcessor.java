a6bfd1232874d6a1c85d671966f1d99a908f8d7f
asrytikov <asrytikov@gmail.com>
2023-12-03 17:00:20 +0400

    Initial commit

---
+++ src/main/java/org/example/processor/CommentProcessor.java
@@ -0,0 +1,2 @@
+package org.example.processor;public class CommentProcessor {
+}
---
+++ src/main/java/org/example/service/CommentService.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.example.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL")CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
